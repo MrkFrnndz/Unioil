8b89d8edd7ff34d8eb48d32edb4b0343aac1dc77
MrkFrnndz <fernandezanthony244@gmail.com>
2018-06-07 17:42:21 +0800

    Jun 7, 2018 5:30 - Added FTP uploading Jun 6, 2018 5:30 - Append on output file Jun 6, 2018 3:00 - Added Call Camera API Jun 6, 2018 2:00 - Added Clear Drawing and Save function Jun 5, 2018 2:30 - Added Signature Activity

--- app/src/main/java/com/example/mark/unioil/SubmitActivity.java
+++ app/src/main/java/com/example/mark/unioil/SubmitActivity.java
@@ -1,14 +1,105 @@
 package com.example.mark.unioil;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
+import android.view.View;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class SubmitActivity extends AppCompatActivity {
+
+    final String FTPHost = "files.000webhost.com";
+    final String user = "attendancemonitor";
+    final String pass = "darksalad12";
+    final int PORT = 21;
+    final int PICK_FILE = 1;
+    AppCompatButton btnUpload, btnSignature, btnPicture;
+    String filename, filepath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
         getSupportActionBar().setTitle("Upload to FTP");
+
+        initialize();
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("file/*");
+                startActivityForResult(i, PICK_FILE);
+            }
+        });
+
+        btnSignature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("file/*");
+                startActivityForResult(i, PICK_FILE);
+            }
+        });
+
+        btnPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("file/*");
+                startActivityForResult(i, PICK_FILE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_FILE && data.getData() != null) {
+            filepath = data.getData().getPath();
+            filename = data.getData().getLastPathSegment();
+            new UploadFile().execute(filepath, FTPHost, user, pass);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void initialize() {
+        btnUpload = (AppCompatButton) findViewById(R.id.btnUpload);
+        btnSignature = (AppCompatButton) findViewById(R.id.btnSignature);
+        btnPicture = (AppCompatButton) findViewById(R.id.btnPicture);
+    }
+
+    private class UploadFile extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            FTPClient client = new FTPClient();
+            try {
+                client.connect(params[1], PORT);
+                client.login(params[2], params[3]);
+                client.enterLocalPassiveMode();
+                client.changeWorkingDirectory("/public_html/CSVFiles");
+                client.setFileType(FTP.BINARY_FILE_TYPE);
+                return client.storeFile(filename, new FileInputStream(new File(params[0])));
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d("FTP", e.toString());
+                return false;
+            } finally {
+                try {
+                    client.logout();
+                    client.disconnect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
